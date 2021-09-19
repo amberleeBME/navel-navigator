b3e92bd317b9001682eefbf993824cb4c6399c84
Amber <amberleeBME@outlook.com>
2021-09-18 21:51:57 -0400

    get json data

--- static/js/app.js
+++ static/js/app.js
@@ -2,8 +2,11 @@
 
 // Read JSON file
 const url="./samples.json";
+const dataPromise =d3.json(url);
+console.log("Data Promise", dataPromise);
 
-d3.json(url, function(data){
+
+d3.json(url).then(function(data){
   // subjectIDs = Object.values(json.names);
   // data = json.metadata;
   console.log(data);
