28a055805d9ac91a218a6835f19ddd076b1c0bcc
Amber <amberleeBME@outlook.com>
2021-09-18 21:25:26 -0400

    get JSON file

---
+++ static/js/app.js
@@ -0,0 +1,14 @@
+var subjectIDs;
+
+// Read JSON file
+const url="samples.json";
+
+d3.json(url, function(json){
+  // subjectIDs = Object.values(json.names);
+  // data = json.metadata;
+  console.log(json);
+
+});
+
+// Create an array of each subject's ID number
+// var subjects = Object.values(data.names);
