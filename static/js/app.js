5d964e3d3288a353059ef1db142bbbb5496c9bd0
Amber <amberleeBME@outlook.com>
2021-09-19 01:21:00 -0400

    lists subjects in dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -1,17 +1,53 @@
 // var subjectIDs;
 
 // Read JSON file
-const url="./samples.json";
-const dataPromise =d3.json(url);
-console.log("Data Promise", dataPromise);
+// const url="https://amberleebme.github.io/plotly-interactive-challenge/samples.json";
+const url=".samples.json";
 
 
-d3.json(url).then(function(data){
-  // subjectIDs = Object.values(json.names);
-  // data = json.metadata;
-  console.log(data);
 
-});
+function init(){
+  d3.json(url).then(function(data){
+  console.log("Sample.json: ",data);
+  let subjectIDs = data.names;
+  let metaData = data.metadata;
+  console.log(metaData);
+  let samples = data.samples[0];
+  listIDs(subjectIDs);
 
-// Create an array of each subject's ID number
-// var subjects = Object.values(data.names);
+  first = d3.selectAll("#selDataset");
+  selection = first.property("value");
+  
+  selectDemo(metaData, selection);
+
+  });
+}
+
+function listIDs(idArray){
+  console.log("Subject IDs: ",idArray);
+  d3.select('select')
+    .selectAll('option')
+    .data(idArray)
+    .enter()
+    .append('option')
+    .text(function(d){ return d;});
+}
+
+
+function selectID(meta){
+  return meta.id === parseInt(this);
+}
+
+function optionChanged(selection){
+  selectDemo(metaData, selection);
+  return selection;
+}
+function selectDemo(var1,var2){
+  
+  subject = var1.filter(selectID,var2)[0];
+  console.log(subject);
+  return var1.filter(selectID,var2);
+  
+}
+init();
+d3.selectAll("#selDataset").on("change", optionChanged);
