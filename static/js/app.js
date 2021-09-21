e59210b4c618ec5935071807a124e2bf0b28edf6
Amber <amberleeBME@outlook.com>
2021-09-20 22:57:00 -0400

    chooses appropriate url and gets selected metadata

--- static/js/app.js
+++ static/js/app.js
@@ -1,51 +1,82 @@
-// var subjectIDs;
+/* Declaring strict mode to help write cleaner code */
+"use strict";
 
-// Read JSON file
-// const url="https://amberleebme.github.io/plotly-interactive-challenge/samples.json";
-const url="samples.json";
+///////// Declare Global Variables \\\\\\\\\\
+var jsonPromise;
+var data;
+var IDs;
+var metaData;
+var samples;
+var specifiedID;
+var url;
+var shortURL = "samples.json";
+var longURL = "https://amberleebme.github.io/plotly-interactive-challenge/samples.json";
 
-function init(){
-  d3.json(url).then(function(data){
-  console.log("Sample.json: ",data);
-  let subjectIDs = data.names;
-  let metaData = data.metadata;
-  console.log(metaData);
-  let samples = data.samples[0];
-  listIDs(subjectIDs);
+///////// Define Functions \\\\\\\\\\
 
-  first = d3.selectAll("#selDataset");
-  selection = first.property("value");
-  
-  selectDemo(metaData, selection);
-
-  });
+/* function getData(): retrieves the data from JSON file */
+function getData(){
+  // If initial load of page, fetch promise and assign to jsonPromise
+  if(jsonPromise === undefined){
+    console.log("***Initial Data Load***");
+    // if running app from server, use local url, else use full url
+    if (window.location.href[0] !="f"){
+      url = shortURL;
+    } else {
+      url = longURL;
+    }
+    console.log(`JSON URL: ${url}`)
+    jsonPromise = d3.json(url);
+    console.log(`Promise Fulfilled!`);
+    return  jsonPromise.then(function(json){
+      // Use jsonPromise to initialize declared arrays.
+      IDs = json.names;
+      metaData = json.metadata;
+      samples = json.samples;
+      data = {
+        subjectIDs:IDs,
+        metaData:metaData,
+        samples:samples
+      };
+      console.log("JSON Keys: ",Object.keys(data));
+      return data;
+    });
+  }
 }
 
 function listIDs(idArray){
-  console.log("Subject IDs: ",idArray);
   d3.select('select')
-    .selectAll('option')
-    .data(idArray)
-    .enter()
-    .append('option')
-    .text(function(d){ return d;});
+      .selectAll('option')
+      .data(idArray)
+      .enter()
+      .append('option')
+      .text(function(d){
+        return d;
+      });
+  console.log(`Total SubjectIDs in Dropdown: ${idArray.length}`);
 }
 
 
-function selectID(meta){
-  return meta.id === parseInt(this);
+function optionChanged(varID){
+  if (varID != undefined){
+  console.log("Test Subject ID No: ",varID);
+  let ind = IDs.indexOf(varID);
+  let obj = metaData[ind];
+  console.log("Specified MetaData: ",obj);
+  return obj;
+  }
 }
 
-function optionChanged(selection){
-  selectDemo(metaData, selection);
-  return selection;
-}
-function selectDemo(var1,var2){
-  
-  subject = var1.filter(selectID,var2)[0];
-  console.log(subject);
-  return var1.filter(selectID,var2);
-  
-}
-init();
-d3.selectAll("#selDataset").on("change", optionChanged);
+///////// Execute Functions \\\\\\\\\\
+getData().then(function(){
+  listIDs(IDs);
+}).then(function(){
+  specifiedID = d3.selectAll("#selDataset").property("value");
+  optionChanged(specifiedID);
+}).then(function(){
+  console.log("***Initial Data Load Complete***");
+});
+// getData().then(function(){
+//   console.log("Data: ", IDs);
+// });
+  d3.selectAll("#selDataset").on("change", optionChanged);
