0c040687483eb5b121140b12aa3f1560657b6751
Amber <amberleeBME@outlook.com>
2021-09-22 23:55:04 -0400

    fixed script placement;added tooltips and badge

--- static/js/app.js
+++ static/js/app.js
@@ -29,7 +29,7 @@ function getData(){
     } else {
       url = longURL;
     }
-    console.log(`JSON URL: ${url}`)
+    console.log(`Promise Sent`)
     jsonPromise = d3.json(url);
     console.log(`Promise Fulfilled!`);
     return  jsonPromise.then(function(json){
@@ -42,7 +42,7 @@ function getData(){
         metaData:metaData,
         samples:samples
       };
-      console.log("Data ",data);
+      console.log("Data Retrieved:",data);
       return data;
     }).then(function(){
       demo = [
@@ -53,13 +53,6 @@ function getData(){
         metaData.map(item =>`${item.location}`),
         metaData.map(item =>`${item.bbtype}`),
         metaData.map(item =>`${item.wfreq}`)
-      // metaData.map(item =>`ID:${item.id}`),
-      // metaData.map(item =>`Ethnicity: ${item.ethnicity}`),
-      // metaData.map(item =>`Gender: ${item.gender}`),
-      // metaData.map(item =>`Age: ${item.age}`),
-      // metaData.map(item =>`Location: ${item.location}`),
-      // metaData.map(item =>`Belly Button Type: ${item.bbtype}`),
-      // metaData.map(item =>`Wash Freq: ${item.wfreq}`)
       ] 
     });
   }
@@ -75,31 +68,29 @@ function listIDs(idArray){
       .text(function(d){
         return d;
       });
-  console.log(`Total SubjectIDs in Dropdown: ${idArray.length}`);
+  console.log(`Dropdown Entries Added: ${idArray.length}`);
 }
 /* function optionChanged(varID): creates/updates visualizations based on selected Test Subject ID */
 function optionChanged(varID){
-  specifiedID =varID;
   let ind = IDs.indexOf(varID);
 
   // Demographics
   {
     d3.select("#demo-info").selectAll("span").remove();
     var col;
-
-    console.log("DEMO: ",demo);
+    var demVals = [];
     demo.forEach((item,i)=>{
       var text = item[ind];
-      
+      demVals.push(text);
       col = `.demo-col-${i+1}`;
-      console.log(col);
       d3.select(col)
           .selectAll("p")
           .append("span")
           .text(text);
     });
-
+    console.log(`Demo Info: ${demVals}`);
   }
+
   // Bar Graph
   {
     var sample = [];
@@ -119,7 +110,6 @@ function optionChanged(varID){
     var sortedSample = sample.sort((a,b) => b.value - a.value);
     var slicedSample = sortedSample.slice(0,10);
     var reversedData = slicedSample.reverse();
-    console.log(reversedData);
     layoutBar = {
       yaxis: {
         automargin: true,
@@ -134,7 +124,7 @@ function optionChanged(varID){
       }
     };
     let color = reversedData.map(object => object.intID);
-    console.log(color);
+    console.log(`Bar Chart Top OTUs: ${color}`);
     let trace1 = {
       x: reversedData.map(object => object.value),
       y: reversedData.map(object => object.id),
@@ -167,7 +157,7 @@ function optionChanged(varID){
       sample2.push(entry);
     }
     var size = sample2.map(object => object.value);
-    console.log(sample2.map(object => object.id));
+    console.log(`Bubble Chart Samples: ${sample2.length}`);
     let trace2 = {
       x: sample2.map(object => object.id),
       y: sample2.map(object => object.value),
@@ -188,7 +178,7 @@ function optionChanged(varID){
     }
     traceData = [trace2];
     let layoutBubble = {
-      title: `All OTUs Found in Participant ${specifiedID}`,
+      title: `All OTUs Found in Participant ${varID}`,
       xaxis: {
         title: {
           text: 'OTU ID #',
@@ -205,10 +195,12 @@ function optionChanged(varID){
   }
   // Gauge Chart
   {
+    var wFVal = metaData[ind].wfreq;
     var data = [
       {
+        
         domain: { x: [0, 1], y: [0, 1] },
-        value: metaData[ind].wfreq,
+        value: wFVal,
         title: { text: `Weekly Wash Frequency`},
         type: "indicator",
         mode: "gauge+number",
@@ -235,6 +227,7 @@ function optionChanged(varID){
         pad: 10
       }
     };
+    console.log(`Wash Freq: ${wFVal}`)
     Plotly.newPlot('gauge', data, layoutGauge, config);
   }
 }
@@ -244,13 +237,14 @@ getData().then(function(){
   listIDs(IDs);
 }).then(function(){
   specifiedID = d3.selectAll("#selDataset").property("value");
+  console.log("Initial Participant:", specifiedID);
   optionChanged(specifiedID);
 }).then(function(){
   console.log("***Initial Data Load Complete***");
 });
 d3.select("#selDataset").on("change", function(){
-  console.log("Changed to: ",this.value);
+  console.log("*Participant Changed to: ",this.value);
   optionChanged(this.value);
 });
-  
+
   
